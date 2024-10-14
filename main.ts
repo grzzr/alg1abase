1ea6526d21ba3ae2d66f49712d47d99968e2385a
grzzr <98114095+grzzr@users.noreply.github.com>
2024-10-14 00:16:48 -0300

    descrição e condição de vitória

--- main.ts
+++ main.ts
@@ -1,3 +1,5 @@
+// Use as funções de movimentação em uma sequência que leve o macaquinho até o acostamento do outro lado da avenida e sem bater em nenhuma árvore.
+// 
 function solucionar () {
 	
 }
@@ -12,29 +14,12 @@ function subir (aSprite: Sprite) {
 function inicializar () {
     scene.setBackgroundImage(assets.image`tabuleiro`)
     tiles.setCurrentTilemap(tilemap`tabuleiro_tiles`)
-    personagem = sprites.create(img`
-        . . . . . . f f f f . . . . . . 
-        . . . . f f f 2 2 f f f . . . . 
-        . . . f f f 2 2 2 2 f f f . . . 
-        . . f f f e e e e e e f f f . . 
-        . . f f e 2 2 2 2 2 2 e e f . . 
-        . . f e 2 f f f f f f 2 e f . . 
-        . . f f f f e e e e f f f f . . 
-        . f f e f b f 4 4 f b f e f f . 
-        . f e e 4 1 f d d f 1 4 e e f . 
-        . . f e e d d d d d d e e f . . 
-        . . . f e e 4 4 4 4 e e f . . . 
-        . . e 4 f 2 2 2 2 2 2 f 4 e . . 
-        . . 4 d f 2 2 2 2 2 2 f d 4 . . 
-        . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
-        . . . . . f f f f f f . . . . . 
-        . . . . . f f . . f f . . . . . 
-        `, SpriteKind.Player)
-    personagem.setStayInScreen(true)
-    grid.place(personagem, tiles.getTileLocation(0, 0))
+    mySprite = sprites.create(assets.image`personagem`, SpriteKind.Player)
+    mySprite.setStayInScreen(true)
+    grid.place(mySprite, tiles.getTileLocation(5, 7))
 }
 function mostrar_tarefa () {
-    game.showLongText("Tarefa:", DialogLayout.Bottom)
+    game.showLongText("Ajude o macaquinho a atravessar a rua.", DialogLayout.Bottom)
 }
 function direita (aSprite: Sprite) {
     grid.move(aSprite, 1, 0)
@@ -44,7 +29,17 @@ function descer (aSprite: Sprite) {
     grid.move(aSprite, 0, 1)
     pause(200)
 }
-let personagem: Sprite = null
+let mySprite: Sprite = null
 inicializar()
 mostrar_tarefa()
 solucionar()
+game.onUpdate(function () {
+    if (grid.spriteRow(mySprite) == 1) {
+        game.gameOver(true)
+        game.setGameOverEffect(true, effects.confetti)
+    }
+    if (tiles.tileAtLocationEquals(grid.getLocation(mySprite), sprites.builtin.forestTiles0)) {
+        game.showLongText("Bateu! Reescreva a solução e tente novamente.", DialogLayout.Bottom)
+        grid.place(mySprite, tiles.getTileLocation(5, 7))
+    }
+})
